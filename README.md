44f659e888ae35daf1bb021cf175c1809972a508
Simon <simonjturk@gmail.com>
2020-01-11 12:49:20 +1100

    graphQL install

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# angularStarter
+# angularStarterPWAGraphQL
 
 TODO: Add documentartion
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularStarter'`, () => {
+  it(`should have as title 'angularStarterPWAGraphQL'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angularStarter');
+    expect(app.title).toEqual('angularStarterPWAGraphQL');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angularStarter!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angularStarterPWAGraphQL!');
   });
 });
