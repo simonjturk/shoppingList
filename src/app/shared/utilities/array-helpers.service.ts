a1fedb8bb2689c4a5c838f341fd7b825d1fee2b3
Simon <simonjturk@gmail.com>
2020-02-12 15:41:11 +1100

    reduced use of lodash

--- src/app/shared/utilities/array-helpers.service.ts
+++ src/app/shared/utilities/array-helpers.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import * as _ from 'lodash';
+
+import { filter } from 'lodash'
 
 export interface IPredicate<T> {
     (item: T): boolean
@@ -7,7 +8,7 @@ export interface IPredicate<T> {
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ArrayHelpersService {
 
@@ -26,20 +27,20 @@ export class ArrayHelpersService {
      * @returns {Array<T>}
      * @memberof ItemsService
      */
-    sort<T>(arr: Array<T>,  key:string, ascending:boolean = true): Array<T> {
-        if (arr){
-            return arr.sort(this.compare(key, ascending ? 'asc': 'desc'));
+    sort<T>(arr: Array<T>, key: string, ascending: boolean = true): Array<T> {
+        if (arr) {
+            return arr.sort(this.compare(key, ascending ? 'asc' : 'desc'));
         }
     }
 
-  
 
-    filter<T>(arr: Array<T>, predicate:IPredicate<T> ){
-        return _.filter(arr,predicate )
+
+    filter<T>(arr: Array<T>, predicate: IPredicate<T>) {
+        return filter(arr, predicate)
     }
 
-    filterByProperty<T>(arr: Array<T>, searchBy: any ){
-      return _.filter(arr,searchBy )
+    filterByProperty<T>(arr: Array<T>, searchBy: any) {
+        return filter(arr, searchBy)
     }
 
 
@@ -64,12 +65,12 @@ export class ArrayHelpersService {
                 // property doesn't exist on either object
                 return 0;
             }
-        
+
             const varA = (typeof a[key] === 'string')
                 ? a[key].toUpperCase() : a[key];
             const varB = (typeof b[key] === 'string')
                 ? b[key].toUpperCase() : b[key];
-        
+
             let comparison = 0;
             if (varA > varB) {
                 comparison = 1;
